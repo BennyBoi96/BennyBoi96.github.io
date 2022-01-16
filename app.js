d7596112b876d19c41c0f3a457c6a4b875eace0e
Flavio Roth <flavio.roth@gmail.com>
2022-01-16 18:35:38 +0100

    Basic functionality code

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-/* code goes here */
+
+
+function compute_score() {
+    // Get all the question elements
+    var question_elements = $("ul.question");
+
+    // Initialize the score
+    var score = 0;
+
+    // Loop through each question
+    question_elements.each(function(index, element){
+        
+        // Check if the correct answer was selected
+        var correct_answer = $(element).find("input[data-correct-answer]").first();
+        if(correct_answer.is(":checked")) {
+            score += 1;
+        }
+    });
+
+    // Display the score
+    alert("Your score is " + score + " / " + question_elements.length);
+}
+
